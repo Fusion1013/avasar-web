878490edfb7b810cb9aa0e2b4870db4554f8c8fe
Fusion1013 <william.reutmer@gmail.com>
2024-06-16 03:17:05 +0200

    Quartz sync: Jun 16, 2024, 3:17 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "fusion1013.github.io/avasar-web",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
